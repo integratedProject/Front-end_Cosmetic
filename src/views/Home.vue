76e595267addac37c416dfb2d9ccde5abbc6dd4d
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-04-28 23:32:11 +0700

    slide show ,editing to tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="home">
+  <div class="box-border">
     <!-- Banner -->
 
-    <div id="banner" class="mx-auto box-content h-5/6 w-5/6">
-      <div id="images">
-        <img :src="bannerLipstick"  class="banner"/>
-        <img :src="bannerEyeshadow" class="banner"/>
-        <img :src="bannerBlush" class="banner"/>
+    <div class="slideshow-container">
+      <div class="mySlides fade" v-for="(slide, index) in slides" :key="index">
+        <img :src="slide" class="w-full" />
       </div>
-
-      <button @click="previousBtn">
-        &lt;
-      </button>
-      <button @click="nextBtn">
-        &gt;
-      </button>
-
-      <about></about>
+      <div class="flex justify-between">
+        <a class="prev" @click="plusSlides(-1)">&lt;</a>
+        <a class="next" @click="plusSlides(1)">&gt;</a>
+      </div>
     </div>
+    <br />
+
+    <div style="text-align:center">
+      <span class="dot" @click="currentSlide(1)"></span>
+      <span class="dot" @click="currentSlide(2)"></span>
+      <span class="dot" @click="currentSlide(3)"></span>
+    </div>
+
+    <about></about>
   </div>
 </template>
 
@@ -31,46 +33,166 @@ export default {
   },
   data() {
     return {
-      bannerLipstick:
+      slides: [
         "https://www.dior.com/couture/var/dior/storage/images/horizon/make-up/lips/lipstick/lipsticks/14203531-93-int-EN/lipsticks_1440_1200.jpg",
-
-      bannerEyeshadow:
         "https://www.dior.com/couture/var/dior/storage/images/14208007/26-int-EN/eyeshadows2_1440_1200.jpg",
-
-      bannerBlush:
         "https://www.dior.com/couture/var/dior/storage/images/14214663/27-int-EN/highlighter-luminizer2_1440_1200.jpg",
-      imageElems: null,
-      currentIndex: 0,
+      ],
+      slideIndex: 1,
     };
   },
   methods: {
-    displayImage(imageElems) {
-      console.log(this.currentIndex);
-      const lastIndex = this.imageElems.length - 1;
- 
+    plusSlides(index) {
+      this.showSlides((this.slideIndex += index));
+    },
 
-      if (this.currentIndex < 0) {       
-        this.currentIndex = lastIndex;
-        
-      } else if (this.currentIndex > lastIndex) {
-        this.currentIndex = 0;
+    currentSlide(index) {
+      this.showSlides((this.slideIndex = index));
+    },
+
+    showSlides(index) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (index > slides.length) {
+        this.slideIndex = 1;
       }
-      const newImage = imageElems[this.currentIndex];
-      newImage.scrollIntoView({ behavior: "smooth" });
-      
-    },
-    previousBtn() {
-      this.currentIndex--;
-      this.displayImage(this.imageElems);
-
-    },
-    nextBtn() {
-      this.currentIndex++;
-      this.displayImage(this.imageElems);
+      if (index < 1) {
+        this.slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[this.slideIndex - 1].style.display = "block";
+      dots[this.slideIndex - 1].className += " active";
     },
   },
   mounted() {
-    this.imageElems = document.querySelectorAll(".banner");
+    this.showSlides(this.slideIndex);
   },
 };
 </script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.mySlides {
+  display: none;
+}
+img {
+  vertical-align: middle;
+}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Next & previous buttons */
+.prev,
+.next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover,
+.next:hover {
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active,
+.dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {
+    opacity: 0.4;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes fade {
+  from {
+    opacity: 0.4;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev,
+  .next,
+  .text {
+    font-size: 11px;
+  }
+}
+</style>
