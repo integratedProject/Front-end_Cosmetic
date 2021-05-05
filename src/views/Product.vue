d8f4eb78453c3d4256e21227afd54336f8957de1
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-05-05 12:08:31 +0700

    editing product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,11 +1,30 @@
 <template>
-  <div id="product">
-    <h1>Product Page</h1>
+  <div id="product" class="grid grid-cols-2 gap-6">
     <!-- picture  -->
-    <div></div>
-    {{productid}}
+    <div>
+      <!-- test path -->
+      <img :src="productimage" />
+      {{ productid }}
+    </div>
+
     <!-- data  -->
-    <div></div>
+    <div class="text-left">
+      <h1>{{ productname }}</h1>
+      <p>{{ description }}</p>
+      <div>
+        <p>{{ price }}</p>
+        <p>Launch: {{ date }}</p>
+        <!-- add to cart  -->
+        Add to cart
+        <div class="flex m-3">
+          <img src="@/assets/shopping-cart.png" class="w-1/12 h-1/12 mx-3" />
+          <span class="border-2 border-black px-5 text-2xl flex items-center">{{ quantity }}</span>
+          <span >+</span>
+          <span>-</span>
+        </div>
+        <div></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -22,6 +41,7 @@ export default {
       description:
         "Rouge Dior is the Dior lipstick with couture colors. True floral lip care, it is enriched with red peony and pomegranate flower extracts of natural origin. Rouge Dior dresses the lips in 75 bold colors with satin, matte, metallic or velvet finishes. Long-wear lipstick that ensures 16 hours of comfort* on the lips.",
       brand_brandid: "B02",
+      quantity: 1,
     };
   },
 };
